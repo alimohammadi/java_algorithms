30295d13cfba19268897bb16d3a434c9804162ba
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-10-04 11:39:50 +0330

    complete resize array method

--- src/com/hubberspot/algorithms/array/ArrayUtil.java
+++ src/com/hubberspot/algorithms/array/ArrayUtil.java
@@ -37,6 +37,16 @@ public class ArrayUtil {
         System.out.println("final for");
     }
 
+    public int[] resizeArray(int[] arr, int capacity){
+        int[] temp = new int[capacity];
+
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+        }
+
+        return temp;
+    }
+
     public static void main(String[] args) {
 //        ArrayUtil arrayUtil = new ArrayUtil();
 //
